be7faa8ad1bef75be513ddd3d533fa553b12e8b2
dkasiviswanath-ai <dkasiviswanath.ai@outlook.com>
2026-05-25 19:14:31 +0530

    Update streamlit_traveladvisor.py

--- streamlit_traveladvisor.py
+++ streamlit_traveladvisor.py
@@ -10,16 +10,26 @@ from langchain_core.prompts import PromptTemplate
 load_dotenv()
 
 prompt_template = PromptTemplate(
-input_variables = ["country"],
-template =""" You are an expert in traditional cuisines. You provide specific dish from a country. Answer the question: What is the traditional cusine of {country}?"""
+input_variables = ["country", "month", "language", "budget"],
+template =""" You are an travel expert and provide recommendations. Consider the inputs for recommendations.
+If the user provide imaginary value for country, just say I don't know.
+ 1. Enter the country: {country}
+ 2. Enter the month: {month}
+ 3. Enter the language: {language}
+ 4. Enter the budget: {budget}
+ ?"""
 )
 
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 llm=ChatOpenAI(model="gpt-4o", api_key=OPENAI_API_KEY, temperature=0.9)
-st.title("Cuisine Info")
+st.title("Travel Recommendations")
 
 country = st.text_input("Enter your country?")
-response = llm.invoke(prompt_template.format(country=country))
+month = st.text_input("Enter the month?")
+language = st.text_input("Enter the language?")
+budget = st.text_input("Enter your budget?")
+
+response = llm.invoke(prompt_template.format(country=country, month=month, language=language, budget=budget))
 
 if country:
     st.write(response.content)
